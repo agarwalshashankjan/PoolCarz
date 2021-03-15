3df8a0cd665f015f59c6ff3ea5124d6c915222b3
Shashank Agarwal <agarwalshashank.jan@gmail.com>
2021-03-15 13:33:55 +0530

    binded login button using dummy data

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  users: { userName: string; password: string }[] = [
+    { userName: 'shashank@prideIndia.org', password: 'waitoka123' },
+    { userName: 'shailesh@prideIndia.org', password: 'hola123' },
+  ];
 
-  constructor() { }
+  myusername:string = '';
+  mypassword:string = '';
 
-  ngOnInit(): void {
+  isValid: boolean = null;
+
+  checkUser() {
+    if (
+      this.myusername == this.users[1].userName &&
+      this.mypassword == this.users[1].password
+    )
+      this.isValid = true;
+    else this.isValid = false;
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
